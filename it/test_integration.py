8244474a9a15084f6855dfd6bcc2fef2da3c83b8
Andrew Crozier <wacrozier@gmail.com>
2018-02-25 19:49:31 +0000

    Test some basic pyspark usage

--- it/test_integration.py
+++ it/test_integration.py
@@ -14,9 +14,19 @@ def livy_available():
     return response.ok
 
 
+PYSPARK_CREATE_DF = """
+from pyspark.sql import Row
+df = spark.createDataFrame([Row(value=i) for i in range(100)])
+"""
+
+
 def test_pyspark(capsys):
     assert livy_available()
 
     with Livy(LIVY_URL) as client:
         client.run('print("foo")')
         assert capsys.readouterr() == ('foo\n', '')
+
+        client.run(PYSPARK_CREATE_DF)
+        client.run('df.count()')
+        assert capsys.readouterr() == ('100\n', '')
