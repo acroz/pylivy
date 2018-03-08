9d730367ec9dd704a780fa52ac1ae42a1968b07e
Andrew Crozier <wacrozier@gmail.com>
2018-03-08 13:46:38 +0000

    Add test for output deserialisation

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,4 +1,6 @@
-from livy.models import Session, SessionKind, SessionState
+from livy.models import (
+    Session, SessionKind, SessionState, Output, OutputStatus
+)
 
 
 def test_session_from_json():
@@ -12,3 +14,18 @@ def test_session_from_json():
     expected_session = Session(5, SessionKind.PYSPARK, SessionState.IDLE)
 
     assert Session.from_json(session_json) == expected_session
+
+
+def test_output_from_json():
+
+    output_json = {
+        'status': 'ok',
+        'data': {'text/plain': 'some output'}
+    }
+
+    expected_output = Output(
+        OutputStatus.OK, text='some output', json=None,
+        ename=None, evalue=None, traceback=None
+    )
+
+    assert Output.from_json(output_json) == expected_output
