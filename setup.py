b6fab8460034343d107ee2d966a8e6adfb596874
Andrew Crozier <wacrozier@gmail.com>
2018-11-28 00:54:10 +0000

    Bump version to 0.4.0

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ class IntegrationTests(PyTest):
 
 setup(
     name='livy',
-    version='0.3.1',
+    version='0.4.0',
     description='A Python client for Apache Livy',
     long_description=README.read_text(),
     packages=['livy'],
