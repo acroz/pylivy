e9c5e6fa681f0cec54aa5c2638649238b6b3303e
Glenn Thomas <gthomas@slack-corp.com>
2020-06-04 11:49:53 -0700

    add tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -58,10 +58,10 @@ def test_session_from_json():
 def test_statement_from_json_no_output():
 
     session_id = 5
-    statement_json = {"id": 10, "state": "running", "output": None}
+    statement_json = {"id": 10, "state": "running", "output": None, "progress": 0.0}
 
     expected = Statement(
-        session_id, statement_id=10, state=StatementState.RUNNING, output=None
+        session_id, statement_id=10, state=StatementState.RUNNING, output=None, progress=0.0
     )
 
     assert Statement.from_json(session_id, statement_json) == expected
@@ -72,17 +72,46 @@ def test_statement_from_json_with_output(mocker):
     mocker.patch.object(Output, "from_json")
 
     session_id = 5
-    statement_json = {"id": 10, "state": "running", "output": "dummy output"}
+    statement_json = {"id": 10, "state": "running", "output": "dummy output", "progress": 0.5}
 
     expected = Statement(
         session_id,
         statement_id=10,
         state=StatementState.RUNNING,
         output=Output.from_json.return_value,
+        progress=0.5
     )
 
     assert Statement.from_json(session_id, statement_json) == expected
     Output.from_json.assert_called_once_with("dummy output")
+
+
+def test_statement_from_json_no_progress(mocker):
+
+    mocker.patch.object(Output, "from_json")
+
+    session_id = 5
+    statement_json = {"id": 10, "state": "running", "output": "dummy output", "progress": None}
+
+    expected = Statement(
+        session_id, statement_id=10, state=StatementState.RUNNING, output=Output.from_json.return_value, progress=None
+    )
+
+    assert Statement.from_json(session_id, statement_json) == expected
+
+
+def test_statement_from_json_with_progress(mocker):
+
+    mocker.patch.object(Output, "from_json")
+
+    session_id = 5
+    statement_json = {"id": 10, "state": "running", "output": "dummy output", "progress": 0.5}
+
+    expected = Statement(
+        session_id, statement_id=10, state=StatementState.RUNNING, output=Output.from_json.return_value, progress=0.5
+    )
+
+    assert Statement.from_json(session_id, statement_json) == expected
 
 
 def test_output_textdata_from_json():
